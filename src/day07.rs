0b28f57812d1c09918b83b36530ac196126998cd
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2023-12-07 21:26:25 +0100

    day7

---
+++ src/day07.rs
@@ -0,0 +1,146 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq,Debug)]
+struct Hand {
+    hand: String,
+    bid: i32,
+    part1: bool
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Hand) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn hand_strength(hand: &Hand) -> i32 {
+    let v: Vec<char> = hand.hand.chars().collect();
+    let mut counts: HashMap<char, i32> = HashMap::new();
+    for c in v {
+        counts.entry(c).or_insert(0);
+        counts.insert(c, counts.get(&c).unwrap() + 1);
+    }
+    if counts.len() == 1 {
+        // five of a kind
+        return 7;
+    }
+    if counts.len() == 2 {
+        if !hand.part1 && counts.contains_key(&'J') {
+            return 7;
+        }
+        for (_, v) in counts.iter() {
+            if *v == 4 {
+                // four of a kind
+                return 6;
+            }
+        }
+        // full house
+        return 5;
+    }
+    if counts.len() == 3 {
+        for (_, v) in counts.iter() {
+            if *v == 3 {
+                if !hand.part1 && counts.contains_key(&'J') {
+                    return 6;
+                }
+                // three of a kind
+                return 4;
+            }
+        }
+        // two pairs
+        if !hand.part1 && counts.contains_key(&'J') {
+            if *(counts.get(&'J').unwrap()) == 2 {
+                return 6;
+            }
+            return 5;
+        }
+        return 3;
+    }
+    if counts.len() == 4 {
+        if !hand.part1 && counts.contains_key(&'J') {
+            return 4;
+        }
+        return 2;
+    }
+    if counts.len() == 5 {
+        if !hand.part1 && counts.contains_key(&'J') {
+            return 2;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Hand) -> Ordering {
+        let s1 = hand_strength(self);
+        let s2 = hand_strength(other);
+        if s1 != s2 {
+            return s1.cmp(&s2);
+        }
+        let h1: Vec<char> = self.hand.chars().collect();
+        let h2: Vec<char> = other.hand.chars().collect();
+        for i in 0..h1.len() {
+            let c1 = h1.get(i).expect("card1");
+            let c2 = h2.get(i).expect("card2");
+            if c1 != c2  {
+                return card_value(*c1, self.part1).cmp(&card_value(*c2, self.part1));
+            }
+        }
+        return Ordering::Equal;
+    }
+}
+
+fn card_value(c: char, part1: bool) -> i32{
+    match c {
+        '2'..='9' => c.to_digit(10).expect("card").try_into().expect("sign"),
+        'T' => 10,
+        'J' => if part1 { 11 } else { 1 },
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => 0,
+    }
+}
+
+
+
+fn parse_hand(line: &str, part1: bool) -> Hand {
+    let mut toks = line.split_whitespace();
+    let hand = toks.next().expect("hand");
+    let bid = toks.next().expect("bidstr").parse::<i32>().expect("bid");
+    return Hand{ hand: hand.to_string(), bid: bid, part1: part1};
+}
+
+pub fn part1(s: String) {
+    let lines = s.lines();
+    let mut hands: Vec::<Hand> = Vec::new();
+    for line in lines {
+        let hand = parse_hand(line, true);
+        hands.push(hand);
+    }
+
+    hands.sort();
+    let mut sum: i32 = 0;
+    for i in 0..hands.len() {
+        sum += TryInto::<i32>::try_into((i+1)).unwrap() * hands.get(i).expect("hand").bid;
+    }
+    println!("{}", sum);
+}
+
+pub fn part2(s: String) {
+    let lines = s.lines();
+    let mut hands: Vec::<Hand> = Vec::new();
+    for line in lines {
+        let hand = parse_hand(line, false);
+        hands.push(hand);
+    }
+
+    hands.sort();
+    let mut sum: i32 = 0;
+    for i in 0..hands.len() {
+        sum += TryInto::<i32>::try_into((i+1)).unwrap() * hands.get(i).expect("hand").bid;
+    }
+    println!("{}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 mod day02;
 mod day03;
 mod day04;
-mod day05;*/
-mod day06;
+mod day05;
+mod day06;*/
+mod day07;
 
 fn main() {
     /*day01::part1(read_test_input(1));
@@ -25,11 +26,15 @@ fn main() {
     day05::part1(read_test_input(5));
     day05::part1(read_input(5));*
     day05::part2(read_test_input(5));
-    day05::part2(read_input(5));*/
+    day05::part2(read_input(5));
     day06::part1(read_test_input(6));
     day06::part1(read_input(6));
     day06::part2(read_test_input(6));
-    day06::part2(read_input(6));
+    day06::part2(read_input(6));*/
+    day07::part1(read_test_input(7));
+    day07::part1(read_input(7));
+    day07::part2(read_test_input(7));
+    day07::part2(read_input(7));
 }
 
 fn read_input(day: usize) -> String {
